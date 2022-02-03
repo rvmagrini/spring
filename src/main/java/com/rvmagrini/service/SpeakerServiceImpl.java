75ca61dfdf1db219f63f611c786a87394a29d4c3
Renato Magrini <renatomagrini@gmail.com>
2022-02-03 08:49:11 +0100

    [4.2] Add Javax Annotation API and Test PostConstructor

--- src/main/java/com/rvmagrini/service/SpeakerServiceImpl.java
+++ src/main/java/com/rvmagrini/service/SpeakerServiceImpl.java
@@ -5,18 +5,19 @@ import com.rvmagrini.repository.SpeakerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
+    @Autowired
     private SpeakerRepository repository;
 
     public SpeakerServiceImpl() {
         System.out.println("SpeakerServiceImpl no args constructor");
     }
 
-    @Autowired
     public SpeakerServiceImpl(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository constructor injection");
         this.repository = repository;
@@ -25,6 +26,11 @@ public class SpeakerServiceImpl implements SpeakerService {
     public void setSpeakerRepository(SpeakerRepository repository) {
         System.out.println("SpeakerServiceImpl repository setter injection");
         this.repository = repository;
+    }
+
+    @PostConstruct
+    private void initialize() {
+        System.out.println("PostConstruct: config called after constructors");
     }
 
     public List<Speaker> findAll() {
